e35e18cfe2d2086ddbeaf82ddaee7f6884ba591d
code-with-paramdeep <paramdeepmuhuri5@gmail.com>
2026-06-22 21:27:17 +0530

    summation problem by loop

---
+++ loop_summation.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int i,j;
+    j=0;
+    for(i=1; i<=10; i++)
+    {
+j=j+i;
+    }
+printf("the summation of the first 10 number is: %d",j);
+    
+    return 0;
+}
