d309b5e550b3718f590438869d33c0a5d0b2035d
code-with-paramdeep <paramdeepmuhuri5@gmail.com>
2026-07-06 21:14:52 +0530

    checking the leap year

---
+++ leap_year.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+int main()
+{
+    int year;
+    printf("enter the year you want to check: ");
+    scanf("%d", &year);
+    if( year%400==0)
+    
+        printf("the year is leap year");
+        else 
+        if( year%4==0)
+        printf("the year is leap year");
+    else 
+    printf("the year is not leap year.");
+return 0;
+
+    
+}
