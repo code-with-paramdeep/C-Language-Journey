df5dfee0772847f275399aa462f43f4cf165fc52
code-with-paramdeep <paramdeepmuhuri5@gmail.com>
2026-08-17 01:02:08 +0530

    without if-else

---
+++ geater.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+int main()
+{
+    int a,b,c;
+printf("enter the first nummber: ");
+scanf("%d",&a);
+printf("enter the second nummber: ");
+scanf("%d",&b);
+c=(a>b)?a:b;
+printf("greater number=%d",c);
+return 0;
+}
