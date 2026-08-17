8443ed25b125a6d041f6b00229b57901a5311abb
code-with-paramdeep <paramdeepmuhuri5@gmail.com>
2026-08-17 20:19:20 +0530

    Shuvam's things

---
+++ Shuvam.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+ float price,final;
+ printf("enter the price");
+ scanf("%f", & price);
+ if(price>=5000)
+ {
+ printf("final price %f", (price - price*0.2));
+ }
+ else if ((price>2000)&&(price<5000))
+ {
+    printf("final price %f",(price-price*0.1));
+ }
+ else
+ {
+  printf("final price %f", (price-price*0.5));
+  
+ }      
+return 0;
+} 
+
