75fadec70ff0a1714bb8fa7c41cae521e6b7521f
code-with-paramdeep <paramdeepmuhuri5@gmail.com>
2026-08-18 01:23:04 +0530

    college assignment

---
+++ equal.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter the number you want to test");
+    scanf("%d", &a);
+    printf("enter the number you want to test");
+    scanf("%d", &b);
+    if(a==b)
+    printf("the nubers are equal");
+    else
+    printf("the numbers are not equal");
+return 0;
+}
